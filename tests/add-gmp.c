c5f3249858b370fd9b867a23ee44fcdf63f90604
n-sm <nsanmartin@psi.uba.ar>
2017-01-22 12:58:04 -0300

    uso gmp para testear

---
+++ tests/add-gmp.c
@@ -0,0 +1,22 @@
+/* Compile with */
+/* gcc -o add_example add_example.c -lgmp -lm */
+
+#include <stdio.h>			/* for printf */
+#include <gmp.h>
+ 
+int main(int argc, char *argv[])
+{
+  mpz_t a, b;                 		/* working numbers */
+  if (argc<3)
+    {					/* not enough words */
+    printf("Please supply two numbers to add.\n");
+    return 1;
+  }
+  mpz_init_set_str (a, argv[1], 10);	/* Assume decimal integers */
+  mpz_init_set_str (b, argv[2], 10);	/* Assume decimal integers */
+  mpz_add (a, a, b);			/* a=a+b */
+
+  printf ("0x%s\n", mpz_get_str (NULL, 16, a));
+  
+  return 0;
+}
